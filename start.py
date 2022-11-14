f259c6b28df18920d102ca588de0ecae6b055d09
coachstevengarcia <coachstevengarcia@gmail.com>
2022-11-13 20:43:36 -0600

    Added attempt number

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ attempt_number = 0
 while user_guess != color and attempt_number <= allowed_attempts:
         user_guess = input("Pick a color: ")
         attempt_number += 1
-        # print("That's attempt number: " + str(attempt_number))
+        print("That's attempt number: " + str(attempt_number))
     
 if attempt_number == allowed_attempts +1:
     print("Sorry to many attempts, YOU LOSE!")
