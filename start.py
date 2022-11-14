ff442d52958f8454c29d5266d1c085412d49de8c
coachstevengarcia <coachstevengarcia@gmail.com>
2022-11-13 20:40:01 -0600

    Initial Commit

---
+++ start.py
@@ -0,0 +1,16 @@
+color = "blue"
+user_guess = ""
+allowed_attempts = 10
+attempt_number = 0
+
+while user_guess != color and attempt_number <= allowed_attempts:
+        user_guess = input("Pick a color: ")
+        attempt_number += 1
+        # print("That's attempt number: " + str(attempt_number))
+    
+if attempt_number == allowed_attempts +1:
+    print("Sorry to many attempts, YOU LOSE!")
+
+elif attempt_number < allowed_attempts:
+    print("YOU WIN!!!!!")
+
